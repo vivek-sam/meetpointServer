afeecfe7d5ecfaa5c01a9c949a429cc989c3a8e2
Vivek Sam <viveksam@gmail.com>
2018-06-15 21:55:34 +0530

    added logging

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var fs = require('fs');
 var express = require('express');
 var https = require('https');
 var bodyParser = require('body-parser');
+const winston = require('winston');
 
-
+const logDir = '../working/log';
 var key = fs.readFileSync('wallet/server-key.pem');
 var cert = fs.readFileSync('wallet/server-crt.pem');
 var ca = fs.readFileSync('wallet/ca-crt.pem');
@@ -25,11 +26,38 @@ function validateToken(token) {
     token = token.trim();
     recognizedToken = recognizedToken.trim();
     
-    console.log(token);
-    console.log(recognizedToken);
+    logger.debug('Token : %s', token);
+    logger.debug('recognized Token : %s', recognizedToken);
 
     return token === recognizedToken;
 }
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+const tsFormat = () => (new Date()).toLocaleTimeString();
+var now = new Date();
+var logfile_name = `${logDir}` + now.getFullYear() + "-"+ now.getMonth() + "-" + now.getDate() +'/server.log';
+
+var logger = new (winston.Logger)({
+    transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      level: 'info'
+    }),
+    new (winston.transports.File)({
+      filename: logfile_name,
+      timestamp: tsFormat,
+      datePattern: 'yyyy-MM-dd',
+      prepend: true,
+      level: 'silly'
+    })
+    ]
+});
 
 app = express();
 
@@ -41,8 +69,8 @@ server = https.createServer(https_options, app).listen(PORT);
 // routes
 app.get('/', function (req, res) {
     res.header('Content-type', 'text/html');
-    return res.end('meetingpoint Server, testing server, just to see a reply');
-    console.log("Received root request");
+    logger.info('Request from IP %s', req.ip);
+    return res.end('meetingpoint Server, testing service, just to see a reply...');
 });
 
 /*
@@ -58,35 +86,38 @@ app.post('/ho', function(req, res) {
 */
 
 app.get('/showvalues', function(req, res) {
-    console.log("Received /showvalues request");
+    logger.info('Received /showvalues request from IP %s', req.ip);
     res.json({gettesting: "123"})
-    res.end();
+    req.end();
 });
 
 app.post('/showhosts', function(req, res) {
-    console.log("Received /showhosts request");
+    logger.info('Received /showhosts request from IP %s', req.ip);
     var token = req.body.token;
     var validationResult = validateToken(token);
-    console.log(validationResult);
+    
     if(validationResult == true) {
-        console.log("Replied /showhosts request");
+        logger.info('Replied /showhosts request');
         res.json({bangalore: "Direct IP"});
     } else {
-        res.json({testing: "123"});
+        res.json({testingshowhosts: "123"});
     }
-    res.end();
+    req.end();
 });
 
 app.post('/addhost', function(req, res) {
-    console.log("Received /addhost request");
+    logger.info('Received /addhost requestfrom IP %s', req.ip);
+
     var token = req.body.token;
     var validationResult = validateToken(token);
+    
     if(validationResult === "true") {
+        logger.info('Replied /addhost request');
         res.json({bangalore: "Direct IP"});
     } else {
-        res.json({testing: "123"});
+        res.json({testingaddhost: "123"});
     }
-    res.end();
+    req.end();
 });
 
-console.log('HTTPS Server listening on %s:%s', HOST, PORT);
+logger.info('HTTPS Server listening on %s:%s', HOST, PORT);
