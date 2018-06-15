f7814025efb80e558c27e31fb0bef6f3294f0daf
Vivek Sam <viveksam@gmail.com>
2018-06-15 13:32:15 +0530

    Update 2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var https = require('https');
 var key = fs.readFileSync('wallet/privateKey.key');
 var cert = fs.readFileSync('wallet/certificate.crt')
+
 var https_options = {
     key: key,
     cert: cert
@@ -11,14 +12,21 @@ var PORT = 7443;
 var HOST = 'viveksam.southindia.cloudapp.azure.com';
 app = express();
 
-server = https.createServer(https_options, app).listen(PORT, HOST);
-console.log('HTTPS Server listening on %s:%s', HOST, PORT);
-
+server = https.createServer(https_options, app).listen(PORT);
 
 // routes
+app.get('/', function (req, res) {
+    res.header('Content-type', 'text/html');
+    return res.end('Hello World!');
+});
+
 app.get('/hey', function(req, res) {
-    res.send('HEY!');
+    res.header('Content-type', 'text/html');
+    return res.end('Hey!');
 });
 app.post('/ho', function(req, res) {
-    res.send('HO!');
+    res.header('Content-type', 'text/html');
+    return res.end('Ho!');
 });
+
+console.log('HTTPS Server listening on %s:%s', HOST, PORT);
