76768756103c0d61c7c7be1bdbabd1bb408855fb
Vivek Sam <viveksam@gmail.com>
2018-06-15 17:49:38 +0530

    add support for json

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
 var express = require('express');
 var https = require('https');
+var bodyParser = require('body-parser');
+
+
 var key = fs.readFileSync('wallet/server-key.pem');
 var cert = fs.readFileSync('wallet/server-crt.pem');
 var ca = fs.readFileSync('wallet/ca-crt.pem');
@@ -13,7 +16,21 @@ var https_options = {
 
 var PORT = 7443;
 var HOST = 'viveksam.southindia.cloudapp.azure.com';
+
+/*
+Functions
+*/
+function validateToken(token) {
+    if(token === "mytoken") {
+        return "mytoken";
+    }
+    return "true";
+}
+
 app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 server = https.createServer(https_options, app).listen(PORT);
 
@@ -36,7 +53,33 @@ app.post('/ho', function(req, res) {
 */
 
 app.get('/showvalues', function(req, res) {
-    res.json({bangalore: "Direct IP"})
-})
+    res.json({gettesting: "123"})
+});
+
+app.post('/showhosts', function(req, res) {
+    
+    var token = req.body.token;
+    var validationResult = validateToken(token);
+    if(validationResult === "true") {
+        res.json({bangalore: "Direct IP"});
+    } else if (validationResult === "mytoken") {
+
+    }
+    else {
+        res.json({testing: "123"});
+    }
+    
+});
+
+app.post('/addhost', function(req, res) {
+
+    var token = req.body.token;
+    if(validateToken(token) === "true") {
+        res.json({bangalore: "Direct IP"});
+    } else {
+        res.json({testing: "123"});
+    }
+
+});
 
 console.log('HTTPS Server listening on %s:%s', HOST, PORT);
